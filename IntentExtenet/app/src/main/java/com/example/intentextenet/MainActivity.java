975a8cc74ba5da32140f8896272f46ae85205ceb
jackpbreilly <jackpbreilly@gmail.com>
2020-02-04 16:58:45 +0000

    complete other two parts

--- IntentExtenet/app/src/main/java/com/example/intentextenet/MainActivity.java
+++ IntentExtenet/app/src/main/java/com/example/intentextenet/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.intentextenet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.EditText;
 
@@ -24,11 +26,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchDial(View v){
-        Intent intent = new Intent(MainActivity.this, BrowserActivity.class);
-        String URL = getURLFromTextbox();
-        intent.putExtra("dataKey", URL);
+        EditText textbox = findViewById(R.id.editText);
+        String num = textbox.getText().toString();
+        Uri number = Uri.parse("tel:"+num);
+        Intent intent = new Intent(Intent.ACTION_DIAL, number);
         startActivity(intent);
     }
+    public void launchContacts(View v){
+        EditText textbox = findViewById(R.id.editText);
+        String num = textbox.getText().toString();
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+        intent.putExtra(ContactsContract.Intents.Insert.PHONE, num);
+        startActivity(intent);
+    }
+
 
     public String getURLFromTextbox(){
         EditText textbox = findViewById(R.id.editText);
